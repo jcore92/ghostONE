90445e5e38d6bc61770907463638bdac0576b44d
Jcore92 <jcore92@mailc.net>
2026-03-29 22:18:02 -0400

    Variable Test.sh

---
+++ MutagenesisX.AppDir/Examples/Variable Test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "app_name: $app_name"
+echo "app_temp_loc: $app_temp_loc"
+echo "pkgmngr_install: $pkgmngr_install"
+echo "pkgmngr_refresh: $pkgmngr_refresh"
+echo "gui_flag: $gui_flag"
+echo "tui_flag: $tui_flag"
+#read -p "Press enter to continue"
